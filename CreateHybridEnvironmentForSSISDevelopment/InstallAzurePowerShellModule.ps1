9d5f77122889bc9d90eef5906aa28013f3753dc9
agent86ray <45230312+agent86ray@users.noreply.github.com>
2019-02-03 15:06:33 -0500

    Initial commit.
    Use this PowerShell script to install the Azure PowerShell module.

---
+++ CreateHybridEnvironmentForSSISDevelopment/InstallAzurePowerShellModule.ps1
@@ -0,0 +1,8 @@
+﻿#  install the Azure PowerShell module
+
+# check the PowerShell version; must be 5.x
+$PSVersionTable.PSVersion
+
+# Install; must be running as administrator
+Install-Module -Name Az -AllowClobber
+
